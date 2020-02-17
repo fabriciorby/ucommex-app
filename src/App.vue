6d984625d404e62e0c251f80b5d689deb166e46e
Fabricio Yamamoto <fabriciorby@hotmail.com>
2020-02-16 22:30:49 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-spacer></v-spacer>
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title>UcommeX</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-content>
+      <Menu />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Menu from "./components/Menu";
+
+export default {
+  name: "App",
+
+  components: {
+    Menu
+  },
+
+  data: () => ({
+  })
+};
+</script>
